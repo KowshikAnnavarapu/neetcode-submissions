8a2fb702a0906dc5e93072606bc20e1e7b34213a
KowshikAnnavarapu <kowshik.annavarapu2006@gmail.com>
2026-06-27 22:32:52 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ""
+        for char in s:
+            if char.isalnum():
+                new += char.lower()
+        reverse = new[::-1]
+        if reverse == new:
+            return True
+        else:
+            return False
